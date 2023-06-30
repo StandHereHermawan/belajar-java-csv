e8cb88ad6d73f7834a184cb3d4ffd7d5e75363fa
StandHereHermawan <nolifegansaya@gmail.com>
2023-06-30 08:38:45 +0700

    CSVTest, CreateCSVWithHeader

--- src/test/java/ariefbelajarjava/csv/CSVTest.java
+++ src/test/java/ariefbelajarjava/csv/CSVTest.java
@@ -6,7 +6,6 @@ import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringWriter;
@@ -52,10 +51,26 @@ public class CSVTest {
         CSVParser parser = new CSVParser(reader, format);
 
         for (CSVRecord record : parser) {
-            System.out.println("First name : "+record.get("First Name"));
-            System.out.println("Middle name : "+record.get("Middle Name"));
-            System.out.println("Last name : "+record.get("Last Name"));
-            System.out.println("Value : "+record.get("Value"));
+            System.out.println("First name : " + record.get("First Name"));
+            System.out.println("Middle name : " + record.get("Middle Name"));
+            System.out.println("Last name : " + record.get("Last Name"));
+            System.out.println("Value : " + record.get("Value"));
         }
+    }
+
+    @Test
+    void createCSVWithHeader() throws IOException {
+        StringWriter writer = new StringWriter();
+
+        CSVFormat format = CSVFormat.DEFAULT.builder()
+                .setHeader("First Name", "Last Name", "Value")
+                .build();
+        CSVPrinter printer = new CSVPrinter(writer, format);
+        printer.printRecord("Arief", "Hermawan", 80);
+        printer.printRecord("Hilmi", "Muharrom", 86);
+        printer.flush();
+
+        String csv = writer.getBuffer().toString();
+        System.out.println(csv);
     }
 }
