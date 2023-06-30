60e900d581525509b423a11db2485bc2781b5ea0
StandHereHermawan <nolifegansaya@gmail.com>
2023-06-30 08:27:25 +0700

    CSVTest, readCSV

--- src/test/java/ariefbelajarjava/csv/CSVTest.java
+++ src/test/java/ariefbelajarjava/csv/CSVTest.java
@@ -1,11 +1,17 @@
 package ariefbelajarjava.csv;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class CSVTest {
 
@@ -20,5 +26,20 @@ public class CSVTest {
 
         String csv = writer.getBuffer().toString();
         System.out.println(csv);
+    }
+
+    @Test
+    void readCSV() throws IOException {
+        Path path = Path.of("sample.csv");
+        Reader reader = Files.newBufferedReader(path);
+
+        CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT);
+
+        for (CSVRecord record : parser) {
+            System.out.println("First name : "+record.get(0));
+            System.out.println("Middle name : "+record.get(1));
+            System.out.println("Last name : "+record.get(2));
+            System.out.println("Value : "+record.get(3));
+        }
     }
 }
